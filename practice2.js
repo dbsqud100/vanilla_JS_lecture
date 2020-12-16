1c5a945961ffe5dbd37f3f2b27b9471dfcb53f2d
dbsqud100 <67494551+dbsqud100@users.noreply.github.com>
2020-12-16 22:17:20 +0900

    Create practice2.js

---
+++ practice2.js
@@ -0,0 +1,18 @@
+const clockContainer = document.querySelector(".js-clock"),
+      clockTitle = clockContainer.querySelector("h1");
+function getTime(){
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const houre = date.getHours();
+    const second = date.getSeconds();
+    clockTitle.innerText = `${houre < 10 ? `0${houre}` : houre}:${minutes < 10 ? `0${minutes}` : minutes}:${second < 10 ? `0${second}` : second}`;
+    //위의 초에 넣은 if는 미니 if이다 오브젝트에 바로 조건을 거 수 있다.
+    //setInterval(함수, 시간) 설정한 함수를 해당 시간마다 실행시키는 함수
+    //시간은 밀리세컨드로 받기 때문에 1000에 1초이다.
+}
+function init(){
+    getTime();
+    setInterval(getTime, 1000);
+}
+
+init();

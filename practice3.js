25f7abb69db7aa14d44ebb905417b6e338c0367c
dbsqud100 <67494551+dbsqud100@users.noreply.github.com>
2020-12-18 22:11:40 +0900

    Update practice3.js

--- practice3.js
+++ practice3.js
@@ -5,6 +5,23 @@ const greeting = document.querySelector(".js-greetings");
 const USER_LS = "currentUser";
 const SHOWING = "showing";
 
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+    //preventDefault로이벤트의 기본값을 막는다.
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING);
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text){
     form.classList.remove(SHOWING);
     greeting.classList.add(SHOWING);
@@ -14,13 +31,13 @@ function paintGreeting(text){
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-
+        askForName();
     }else{
         paintGreeting(currentUser);
     }
 }
 function init(){
-    loadName
+    loadName();
 }
 
 init();

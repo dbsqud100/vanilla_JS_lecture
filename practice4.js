f99189d1c83843ade5709a88ac9d85905220db89
dbsqud100 <67494551+dbsqud100@users.noreply.github.com>
2020-12-19 21:57:26 +0900

    Update practice4.js

--- practice4.js
+++ practice4.js
@@ -4,15 +4,31 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = `toDos`;
 
+const toDos = [];
+
+function saveToDos(){
+    //JSON.stringify는 자바스크립트 object를 string으로 바꿔준다.
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+//자바스크립트는 local storage에 있는 모든 데이터를 string으로 저장한다.
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "X";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = "X";
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
     toDoList.appendChild(li);
+    li.id = newId;
+    const toDoObj = {
+        text: text,
+        id : newId
+    };
+    //배열에 값을 넣는 방법
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
@@ -21,11 +37,19 @@ function handleSubmit(event){
     paintToDo(currentValue);
     toDoInput.value = "";
 }
-
+function somthing(text){
+    paintToDo(text.text);
+}
 function loadToDos(){
     const toDos = localStorage.getItem(TODOS_LS);
     if(toDos !== null){
-
+        const parsedToDos = JSON.parse(toDos);
+        //forEach안에 함수를 선언할 수도 있다.
+        // parsedToDos.forEach(function(Todo){
+        //     console.log(Todo.text);
+        // });
+        //아래와 같이 function을 따로 선언하는 것도 가능하다.
+        parsedToDos.forEach(somthing);
     }
 }
 

0bb1d0f4e2efca8d01458ae624f6b70b88aca476
dbsqud100 <67494551+dbsqud100@users.noreply.github.com>
2021-01-06 23:27:54 +0900

    Update practice2.js

--- practice2.js
+++ practice2.js
@@ -7,6 +7,7 @@ function getTime(){
     const second = date.getSeconds();
     clockTitle.innerText = `${houre < 10 ? `0${houre}` : houre}:${minutes < 10 ? `0${minutes}` : minutes}:${second < 10 ? `0${second}` : second}`;
     //위의 초에 넣은 if는 미니 if이다 오브젝트에 바로 조건을 거 수 있다.
+    //미니 if문은 condition ? block(true) : block(false) 인걸 기억하기
     //setInterval(함수, 시간) 설정한 함수를 해당 시간마다 실행시키는 함수
     //시간은 밀리세컨드로 받기 때문에 1000에 1초이다.
 }
